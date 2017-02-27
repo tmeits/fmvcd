edc22cc379b010373f24b17f97c53a04766440c1
tmeits <tmeits@gmail.com>
2017-02-27 10:32:21 +0700

    Update caret-nnet.r

--- caret-nnet.r
+++ caret-nnet.r
@@ -1,11 +1,16 @@
 #
 require(zoo)
 rm(list=ls())
-#sink
+setZeroNegativeNumber <- function(n){
+  if(n < 0) return(0)
+  else return(n)
+}
 filled_path <- "C:/Users/IVA/Dropbox/Apps/wmoID/24763"
+filled_path <- "/home/larisa/Dropbox/Apps/wmoID/24763-2"
 setwd(filled_path); getwd()
 
 file_list <- list.files(pattern = "\\.cli$"); file_list
+sink(paste(filled_path,"/log/","logofcode.txt", sep=""))
 for (file_name in file_list){
   cat("**************  ", file_name, "  *****************\n")
   cli_dataset <-read.table(file_name, header=FALSE, sep="")
@@ -17,17 +22,20 @@ for (file_name in file_list){
   days <- c(1:dim(cli_dataset)[1])
   prec_zoo <- zoo(prec_vector, days)
   temp_zoo <- zoo(temp_vector, days)
-  prec_filled <- as.integer(na.spline(prec_zoo))
+  prec_filled <- sapply(as.integer(na.spline(prec_zoo)), setZeroNegativeNumber)
   temp_filled <- as.integer(na.spline(temp_zoo))
   print(str(prec_filled)); print(str(temp_filled))
   print(summary(prec_filled)); print(summary(temp_filled))
-  new_cli <- data.frame(cli_dataset[,1], cli_dataset[,2], cli_dataset[,3], prec_filled, temp_filled)
+  new_cli <- data.frame(cli_dataset[,1], cli_dataset[,2], cli_dataset[,3], 
+                        prec_filled, temp_filled)
   write.table(new_cli, file = paste(filled_path,"/filled/",file_name, sep=""),
               row.names=FALSE, col.names=FALSE, sep="\t")
   png(filename=paste(filled_path,"/png/perc-",file_name, ".png", sep=""))
   #par(mfrow=c(1,2))
-  plot(prec_filled, main=file_name, col="red"); points(days, prec_vector, col="blue"); dev.off()
+  plot(prec_filled, main=file_name, xlab="Days", col="red", pch=19); points(days, prec_vector, col="blue", pch=19); dev.off()
   png(filename=paste(filled_path,"/png/temp-",file_name, ".png", sep=""))
-  plot(temp_filled, main=file_name, col="red"); points(days, temp_vector, col="blue"); dev.off()
+  plot(temp_filled, main=file_name, xlab="Days", col="red", pch=19); points(days, temp_vector, col="blue", pch=19); dev.off()
 }
+sink()
+# https://www.analyticsvidhya.com/blog/2016/12/practical-guide-to-implement-machine-learning-with-caret-package-in-r-with-practice-problem/
 # https://gist.github.com/anonymous/6c738ab40823ce8466a942897850d262
